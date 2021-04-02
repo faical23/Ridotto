7b632dc8d02ed2f8d9b08507c8bbeeccd6d4820a
Faical Bahsis <65980103+faical23@users.noreply.github.com>
2021-04-02 10:52:03 +0100

    add json file

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,109 +48,77 @@
     }
 
 
-    var json = {
-        pack: [{
-                pack_name: "Istanbul",
-                path_img: "assets/img/pack/istanbul.jpg",
-                apr: "+80%",
-                apr_number: 80
-            },
-            {
 
-                pack_name: "Chafchaouin",
-                path_img: "assets/img/pack/chefchaouin.jpg ",
-                apr: "+36%",
-                apr_number: 36
-            },
-            {
 
-                pack_name: "Madrid",
-                path_img: "assets/img/pack/madrir.jpg ",
-                apr: "+66%",
-                apr_number: 66
-            },
-            {
 
-                pack_name: "Paris ",
-                path_img: "assets/img/pack/paris.jpg ",
-                apr: "+50%",
-                apr_number: 50
-            },
-            {
+    $.getJSON("assets/js/pack_data.json", function(json) {
 
-                pack_name: "Ryo",
-                path_img: "assets/img/pack/ryo.jpg ",
-                apr: "+20%",
-                apr_number: 20
+        ////////////: pack price
+
+        let next_btn = document.querySelector(".next");
+        let back_btn = document.querySelector(".back");
+        let img_slider = document.querySelector(".img_slider");
+        let name_pack = document.querySelector(".name_pack");
+        let apr_pack = document.querySelector(".apr_value");
+        let daily = document.querySelector(".daily_total");
+        let monthly = document.querySelector(".monthly_total");
+        let yearly = document.querySelector(".yearly_total");
+        let daily_ridotto = 0.10;
+        let monthly_ridotto = 3;
+        let yearly_ridotto = 36;
+
+
+        let z = 0;
+
+        const change_pack = (number) => {
+            name_pack.innerHTML = json.pack[number].pack_name
+            img_slider.src = json.pack[number].path_img;
+            apr_pack.innerHTML = json.pack[number].apr
+        }
+
+        const daily_price = (apr, ridotto_prix) => {
+            let res;
+            res = ridotto_prix * 100 + ridotto_prix * apr;
+            return res + "$";
+
+        }
+
+
+        change_pack(z);
+        daily.innerHTML = daily_price(parseInt(json.pack[z].apr), daily_ridotto)
+        monthly.innerHTML = daily_price(parseInt(json.pack[z].apr), monthly_ridotto)
+        yearly.innerHTML = daily_price(parseInt(json.pack[z].apr), yearly_ridotto)
+
+
+
+        next_btn.addEventListener("click", function() {
+            let lengh = json.pack.length
+            if (z < lengh - 1) {
+                z++;
+                change_pack(z);
+                daily.innerHTML = daily_price(parseInt(json.pack[z].apr), daily_ridotto)
+                monthly.innerHTML = daily_price(parseInt(json.pack[z].apr), monthly_ridotto)
+                yearly.innerHTML = daily_price(parseInt(json.pack[z].apr), yearly_ridotto)
+
             }
-        ]
-    }
-
-
-    ////////////: pack price
-
-    let next_btn = document.querySelector(".next");
-    let back_btn = document.querySelector(".back");
-    let img_slider = document.querySelector(".img_slider");
-    let name_pack = document.querySelector(".name_pack");
-    let apr_pack = document.querySelector(".apr_value");
-    let daily = document.querySelector(".daily_total");
-    let monthly = document.querySelector(".monthly_total");
-    let yearly = document.querySelector(".yearly_total");
-    let daily_ridotto = 0.10;
-    let monthly_ridotto = 3;
-    let yearly_ridotto = 36;
-
-
-    let z = 0;
-
-    const change_pack = (number) => {
-        name_pack.innerHTML = json.pack[number].pack_name
-        img_slider.src = json.pack[number].path_img;
-        apr_pack.innerHTML = json.pack[number].apr
-    }
-
-    const daily_price = (apr, ridotto_prix) => {
-        let res;
-        res = ridotto_prix * 100 + ridotto_prix * apr;
-        return res + "$";
-
-    }
-
-
-    change_pack(z);
-    daily.innerHTML = daily_price(parseInt(json.pack[z].apr), daily_ridotto)
-    monthly.innerHTML = daily_price(parseInt(json.pack[z].apr), monthly_ridotto)
-    yearly.innerHTML = daily_price(parseInt(json.pack[z].apr), yearly_ridotto)
+        })
+        back_btn.addEventListener("click", function() {
+            if (z > 0) {
+                z--;
+                change_pack(z);
+                daily.innerHTML = daily_price(parseInt(json.pack[z].apr), daily_ridotto)
+                monthly.innerHTML = daily_price(parseInt(json.pack[z].apr), monthly_ridotto)
+                yearly.innerHTML = daily_price(parseInt(json.pack[z].apr), yearly_ridotto)
+            }
+        })
 
 
 
-    next_btn.addEventListener("click", function() {
-        let lengh = json.pack.length
-        if (z < lengh - 1) {
-            z++;
-            change_pack(z);
-            daily.innerHTML = daily_price(parseInt(json.pack[z].apr), daily_ridotto)
-            monthly.innerHTML = daily_price(parseInt(json.pack[z].apr), monthly_ridotto)
-            yearly.innerHTML = daily_price(parseInt(json.pack[z].apr), yearly_ridotto)
+        /////////////:: range
 
+        let range = document.querySelector("#range_input");
+        range.oninput = function look() {
+            console.log(range.value);
         }
-    })
-    back_btn.addEventListener("click", function() {
-        if (z > 0) {
-            z--;
-            change_pack(z);
-            daily.innerHTML = daily_price(parseInt(json.pack[z].apr), daily_ridotto)
-            monthly.innerHTML = daily_price(parseInt(json.pack[z].apr), monthly_ridotto)
-            yearly.innerHTML = daily_price(parseInt(json.pack[z].apr), yearly_ridotto)
-        }
-    })
 
-
-
-    /////////////:: range
-
-    let range = document.querySelector("#range_input");
-    range.oninput = function look() {
-        console.log(range.value);
-    }
+    });
